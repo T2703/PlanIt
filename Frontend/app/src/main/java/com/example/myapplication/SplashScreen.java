48fdeb156fd93661207b1c3e70e2c1fc3535b525
melanihodge <mhodge.8765@gmail.com>
2023-10-29 15:56:36 -0500

    chore: fix splash screen

--- Frontend/app/src/main/java/com/example/myapplication/SplashScreen.java
+++ Frontend/app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -2,13 +2,48 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import profile.LoginFormPage;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    // Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        // Hooks
+        image = findViewById(R.id.image);
+        logo = findViewById(R.id.logo);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginFormPage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
