6c29eb99117397adfa848b76e8d9228b800930df
melanihodge <mhodge.8765@gmail.com>
2023-10-31 14:09:56 -0500

    chore: add a POST method for logging in

--- Frontend/app/src/main/java/profile/LoginFormPage.java
+++ Frontend/app/src/main/java/profile/LoginFormPage.java
@@ -8,12 +8,28 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.myapplication.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import events.CreateEventPage;
+import events.EventsListViewer;
 import homepage.HomePage;
 
 public class LoginFormPage extends AppCompatActivity {
+
+    private static final String URL_POST_REQUEST = "http://coms-309-024.class.las.iastate.edu:8080/login";
 
     private TextView create_account_button;
     private Button login_button;
@@ -21,7 +37,7 @@ public class LoginFormPage extends AppCompatActivity {
     /*
     The email that the user will use for logging in.
      */
-    private EditText user_email;
+    private EditText user_username;
 
     /*
     The password that the user will use.
@@ -36,7 +52,7 @@ public class LoginFormPage extends AppCompatActivity {
         create_account_button = findViewById(R.id.create_account_button);
         login_button = findViewById(R.id.login_button);
         login_button.setEnabled(false); // Set this to false for checking the inputs of the user.
-        user_email = findViewById(R.id.login_username_field);
+        user_username = findViewById(R.id.login_username_field);
         user_password = findViewById(R.id.login_password_field);
 
         create_account_button.setOnClickListener(new View.OnClickListener() {
@@ -50,13 +66,15 @@ public class LoginFormPage extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginFormPage.this, HomePage.class);
-                startActivity(intent);
+                String username = user_username.getText().toString();
+                String password = user_password.getText().toString();
+
+                sendPostRequest(username, password);
             }
         });
 
         // This is for the user's email to see if anything is in it or not.
-        user_email.addTextChangedListener(new TextWatcher() {
+        user_username.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -86,8 +104,41 @@ public class LoginFormPage extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
             }
         });
+    }
 
+    private void sendPostRequest(String username, String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JSONObject body = new JSONObject();
 
+        try {
+            body.put("username", username);
+            body.put("password", password);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Make the request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                URL_POST_REQUEST,
+                body,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Intent intent = new Intent(LoginFormPage.this, HomePage.class);
+                        startActivity(intent);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Username or password is incorrect.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
     }
 
     /*
@@ -95,7 +146,7 @@ public class LoginFormPage extends AppCompatActivity {
     if there's nothing in both the email and password then, it shouldn't go through.
     */
     private void checkFieldsForEmptyValues() {
-        String user_email_login = user_email.getText().toString();
+        String user_email_login = user_username.getText().toString();
         String password = user_password.getText().toString();
 
         if (!user_email_login.isEmpty() && !password.isEmpty()) {
