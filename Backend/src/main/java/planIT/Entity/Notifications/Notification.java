81476243da28051b1b8729b50ca2b23f88ae743b
Chris Smith <christopherlsmith@live.com>
2023-11-13 17:58:32 -0600

    asdfasd

--- Backend/src/main/java/planIT/Entity/Notifications/Notification.java
+++ Backend/src/main/java/planIT/Entity/Notifications/Notification.java
@@ -12,11 +12,10 @@ import jakarta.persistence.JoinColumn;
 import planIT.Entity.Users.User;
 
 /**
- *
+ * Notifcations entity
  * @author Melani Hodge
  *
  */
-
 @Entity
 public class Notification {
 
@@ -36,25 +35,53 @@ public class Notification {
     @JoinColumn(name = "user_id")
     private User user;
 
-
+    /**
+     * Notification constructor with parameters
+     * @param title notification title
+     * @param description notification description
+     */
     public Notification(String title, String description) {
         this.title = title;
         this.description = description;
     }
 
     // Notification constructor (without parameters)
+
+    /**
+     * Notification constructor without parameters
+     */
     public Notification() { }
 
     /* =============== GETTER & SETTER FUNCTIONS =============== */
 
+    /**
+     * Gets the notification id number
+     * @return id
+     */
     public int getId() { return id; }
 
+    /**
+     * Sets notification id number
+     * @param id id number
+     */
     public void setId(int id){ this.id = id; }
 
+    /**
+     * Gets the notification title
+     * @return title
+     */
     public String getTitle(){ return title; }
 
+    /**
+     * Sets notification title
+     * @param title notification title
+     */
     public void setTitle(String title){ this.title = title; }
 
+    /**
+     * Gets the notification description
+     * @return description
+     */
     public String getDescription() { return description; }
 
     public void setDescription(String description) { this.description = description; }
