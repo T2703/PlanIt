2fb667af8784e88230c9ccc9953de02ee967a528
Chris Smith <christopherlsmith@live.com>
2023-11-13 13:52:36 -0600

    Backend - javadoc for Messages package

--- Backend/src/main/java/planIT/Entity/Messages/Message.java
+++ Backend/src/main/java/planIT/Entity/Messages/Message.java
@@ -11,7 +11,7 @@ import java.util.List;
 import jakarta.persistence.*;
 
 /**
- *
+ * Message entity for containing message info within a chat
  * @author Melani Hodge
  *
  */
@@ -40,6 +40,13 @@ public class Message {
     private Date receiveTime;
 
     // Message constructor (with parameters)
+
+    /**
+     * Constructor for message
+     * @param body text content of a message
+     * @param sendTime time message was sent
+     * @param receiveTime time message was received
+     */
     public Message(String body, Date sendTime, Date receiveTime) {
         this.body = body;
         this.sendTime = sendTime;
@@ -47,30 +54,66 @@ public class Message {
     }
 
     // Message constructor (without parameters)
+
+    /**
+     * Empty message constructor
+     */
     public Message() { }
 
     /* =============== GETTER & SETTER FUNCTIONS =============== */
 
+    /**
+     * Returns message id numbers
+     * @return id
+     */
     public int getId() { return id; }
 
+    /**
+     * Sets the message id number
+     * @param id new id number for message
+     */
     public void setId(int id){
         this.id = id;
     }
 
+    /**
+     * Returns the message body
+     * @return body
+     */
     public String getBody(){
         return body;
     }
 
+    /**
+     * Sets the body of the message
+     * @param body new message body
+     */
     public void setBody(String body){
         this.body = body;
     }
 
+    /**
+     * Returns time the message was sent
+     * @return sendTime
+     */
     public Date getSendTime() { return sendTime; }
 
+    /**
+     * Sets the time message was sent
+     * @param sendTime time message was sent
+     */
     public void setSendTime(Date sendTime) { this.sendTime = sendTime; }
 
+    /**
+     * Gets the time message was received
+     * @return receiveTime
+     */
     public Date getReceiveTime() { return receiveTime; }
 
+    /**
+     * Sets the time message was received
+     * @param receiveTime time message was received
+     */
     public void setReceiveTime(Date receiveTime) { this.receiveTime = receiveTime; }
 
 }
