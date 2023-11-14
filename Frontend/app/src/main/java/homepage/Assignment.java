a5c62f41dafe82770fb359248894450340bb4510
Joshua Gutierrez Mejia <joshuagutierrezmejia@josh1015.student.iastate.edu>
2023-11-13 18:49:14 -0600

    chore: added javadocs for homepage

--- Frontend/app/src/main/java/homepage/Assignment.java
+++ Frontend/app/src/main/java/homepage/Assignment.java
@@ -1,22 +1,66 @@
 package homepage;
 
+/**
+ * A class representing an assignment.
+ *
+ * @author Joshua Gutierrez
+ *
+ * <p>Provides information about the assignment, including the class it belongs to,
+ * its title, and the due date.
+ * </p>
+ */
 public class Assignment {
+    /**
+     * The class to which the assignment belongs.
+     */
     private String assignmentClass;
+
+    /**
+     * The title of the assignment.
+     */
     private String assignmentTitle;
+
+    /**
+     * The due date of the assignment.
+     */
     private String assignmentDueDate;
 
+    /**
+     * Constructs an assignment with the given parameters.
+     *
+     * @param assignmentClass The class to which the assignment belongs.
+     * @param assignmentTitle The title of the assignment.
+     * @param assignmentDueDate The due date of the assignment.
+     */
     public Assignment(String assignmentClass, String assignmentTitle, String assignmentDueDate) {
         this.assignmentClass = assignmentClass;
         this.assignmentTitle = assignmentTitle;
         this.assignmentDueDate = assignmentDueDate;
     }
 
+    /**
+     * Gets the class to which the assignment belongs.
+     *
+     * @return The assignment class.
+     */
     public String getAssignmentClass() {
         return assignmentClass;
     }
+
+    /**
+     * Gets the title of the assignment.
+     *
+     * @return The assignment title.
+     */
     public String getAssignmentTitle() {
         return assignmentTitle;
     }
+
+    /**
+     * Gets the due date of the assignment.
+     *
+     * @return The due date.
+     */
     public String getDueDate() {
         return assignmentDueDate;
     }
--- Frontend/app/src/main/java/homepage/UserAdapter.java
+++ Frontend/app/src/main/java/homepage/UserAdapter.java
@@ -12,13 +12,31 @@ import com.example.myapplication.R;
 
 import java.util.List;
 
+/**
+ * @author Joshua Gutierrez
+ * The UserAdapter class is a RecyclerView adapter for displaying a list of users
+ * with avatars and online status in a RecyclerView.
+ */
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
     private List<User> userList;
 
+    /**
+     * Constructs a new UserAdapter with the specified list of users.
+     *
+     * @param userList The list of users to be displayed in the RecyclerView.
+     */
     public UserAdapter(List<User> userList) {
         this.userList = userList;
     }
 
+    /**
+     * Called when RecyclerView needs a new UserViewHolder to represent
+     * an item with the given type.
+     *
+     * @param parent   The ViewGroup into which the new View will be added.
+     * @param viewType The type of the new View.
+     * @return A new UserViewHolder that holds a View representing an item.
+     */
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -26,6 +44,12 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         return new UserViewHolder(itemView);
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     *
+     * @param holder   The UserViewHolder that holds the View for each item.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(UserViewHolder holder, int position) {
         User user = userList.get(position);
@@ -39,15 +63,28 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         }
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in the adapter's data set.
+     */
     @Override
     public int getItemCount() {
         return userList.size();
     }
 
+    /**
+     * The UserViewHolder class represents a ViewHolder for user items in the RecyclerView.
+     */
     public static class UserViewHolder extends RecyclerView.ViewHolder {
         ImageView avatarImageView;
         ImageView onlineIndicatorImageView;
 
+        /**
+         * Constructs a new UserViewHolder with the specified item view.
+         *
+         * @param itemView The View for each item in the RecyclerView.
+         */
         public UserViewHolder(View itemView) {
             super(itemView);
             avatarImageView = itemView.findViewById(R.id.userAvatar);
