83be894bce00a0fe9246372e5b1dfcf5badb0478
melanihodge <mhodge.8765@gmail.com>
2023-11-13 18:58:14 -0600

    chore: java docs for login, todo, user, application

--- Backend/src/main/java/planIT/PlanITApplication.java
+++ Backend/src/main/java/planIT/PlanITApplication.java
@@ -5,10 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+/**
+ * The main class for the PlanIT application.
+ * This class configures and launches the Spring Boot application.
+ * It excludes security auto-configuration and enables transaction management.
+ * The application can be started by running the {@link #main(String[])} method.
+ *
+ * @author Melani Hodge
+ * @author Chris Smith
+ *
+ */
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableTransactionManagement
 public class PlanITApplication {
 
+    /**
+     * The main entry point for the PlanIT application.
+     * It starts the Spring Boot application.
+     *
+     * @param args Command line arguments passed to the application.
+     */
     public static void main(String[] args) {
         SpringApplication.run(PlanITApplication.class, args);
     }
