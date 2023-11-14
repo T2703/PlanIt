c7d2330c7ac93c2886c4cf2c1fdbe5387f1e47bf
melanihodge <mhodge.8765@gmail.com>
2023-11-13 19:09:32 -0600

    chore: java docs for schedule analysis

--- Backend/src/main/java/planIT/ScheduleAnalysis/scheduleAnalysisController.java
+++ Backend/src/main/java/planIT/ScheduleAnalysis/scheduleAnalysisController.java
@@ -8,16 +8,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import planIT.Entity.Users.*;
 
+/**
+ * Controller class for handling schedule analysis requests.
+ * It provides an endpoint for measuring and displaying the weekly activity of a user based on their schedule.
+ *
+ * @author Chris Smith
+ *
+ */
 @RestController
 public class scheduleAnalysisController {
 
     private final UserRepository userRepository;
 
+    /**
+     * Constructor for scheduleAnalysisController.
+     *
+     * @param userRepository The repository for accessing user data.
+     */
     @Autowired
     public scheduleAnalysisController(UserRepository userRepository){
         this.userRepository = userRepository;
     }
 
+    /**
+     * Endpoint for measuring and displaying the weekly activity of a user based on their schedule.
+     *
+     * @param userId The unique identifier of the user for whom to measure weekly activity.
+     * @return A formatted string representing the weekly activity.
+     */
     @GetMapping(path = "/scheduleAnalysis/{userId}")
     public String test(@PathVariable int userId) {
 
