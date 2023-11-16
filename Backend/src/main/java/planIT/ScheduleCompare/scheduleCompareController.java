7a8b3875b8f99a200aa03ed354db6f138ab572d3
Chris Smith <christopherlsmith@live.com>
2023-11-16 13:35:31 -0600

    Backend - fixed add user to team method

--- Backend/src/main/java/planIT/ScheduleCompare/scheduleCompareController.java
+++ Backend/src/main/java/planIT/ScheduleCompare/scheduleCompareController.java
@@ -1,9 +1,7 @@
 package planIT.ScheduleCompare;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import planIT.Entity.Events.Event;
 import planIT.Entity.Teams.Team;
 import planIT.Entity.Teams.TeamRepository;
