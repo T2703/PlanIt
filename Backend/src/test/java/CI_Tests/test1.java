dc8966f0985f33fa97567d57886cda612cde600b
Chris Smith <christopherlsmith@live.com>
2023-11-19 14:31:57 -0600

    Backend - added chat tests

--- Backend/src/test/java/CI_Tests/test1.java
+++ Backend/src/test/java/CI_Tests/test1.java
@@ -12,7 +12,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,12 +31,12 @@ public class test1 {
     }
 
     @Test
-    public void testChat(){
+    public void testCreateChat(){
 
         Response response = RestAssured.given().
                 header("Content-Type", "JSON").
                 header("charset", "utf-8").
-                body("{  \"name\": \"chat1\"}").
+                body("{\"name\": \"chat1\"}").
                 when().
                 post("/chats");
 
@@ -54,5 +53,141 @@ public class test1 {
         }
     }
 
+    @Test
+    public void testCreateTeamChat(){ //TODO create team within test?
+        Response response = RestAssured.given().
+                header("Content-Type", "JSON").
+                header("charset", "utf-8").
+                body("{\"name\": \"teamChat1\"}").
+                when().
+                post("/chats/1/chat");
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String returnString = response.getBody().asString();
+        try{
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("{\"message\":\"success\"}", returnObj.get("data"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCreateMessageInChat(){
+        Response response = RestAssured.given().
+                header("Content-Type", "JSON").
+                header("charset", "utf-8").
+                body("\"body\": \"MESSAGE 1\",\n" +
+                        "\"sendTime\": null,\n" +
+                        "\"receiveTime\": null").
+                when().
+                post("/chats/1/messages");
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String returnString = response.getBody().asString();
+        try{
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("{\"message\":\"success\"}", returnObj.get("data"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testUpdateChat(){
+
+        Response response = RestAssured.given().
+                header("Content-Type", "JSON").
+                header("charset", "utf-8").
+                body("{\"name\": \"chat1UPDATED\"}").
+                when().
+                put("/chats");
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String returnString = response.getBody().asString();
+        try{
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("{\"name\": \"chat1UPDATED\"}", returnObj.get("data"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAddUserToChat(){
+        Response response = RestAssured.given().
+                header("Content-Type", "JSON").
+                header("charset", "utf-8").
+                //body("{\"name\": \"chat1\"}").
+                when().
+                put("/chats/1/users/A"); //TODO update variables...make sure user exists...and the chat exists
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String returnString = response.getBody().asString();
+        try{
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("{\"message\":\"success\"}", returnObj.get("data"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestDeleteChat(){
+
+        Response response = RestAssured.given().
+                header("Content-Type", "JSON").
+                header("charset", "utf-8").
+                //body("{\"name\": \"chat1\"}").
+                when().
+                delete("/chats/1");
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String returnString = response.getBody().asString();
+        try{
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("{\"message\":\"success\"}", returnObj.get("data"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testRemoveUserFromChat(){
+
+        Response response = RestAssured.given().
+                header("Content-Type", "JSON").
+                header("charset", "utf-8").
+                //body("{\"name\": \"chat1\"}").
+                when().
+                delete("/chats/1/users/A"); //TODO make sure chat and user exist
+
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        String returnString = response.getBody().asString();
+        try{
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("{\"message\":\"success\"}", returnObj.get("data"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
 
 }
