1dd289632719dee54ac7be386e2e3bb25c31bf62
Joshua Gutierrez Mejia <joshuagutierrezmejia@josh1015.student.iastate.edu>
2023-11-27 17:18:01 -0600

    chore: set up test dependencies

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -39,8 +39,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.cardview:cardview:1.0.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
