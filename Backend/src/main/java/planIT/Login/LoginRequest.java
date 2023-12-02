cb70174a292fcb488098949f437f4fe69fb47bb4
melanihodge <mhodge.8765@gmail.com>
2023-12-02 16:30:22 -0600

    chore: complete users system test and finish setting up for tests

--- Backend/src/main/java/planIT/Login/LoginRequest.java
+++ Backend/src/main/java/planIT/Login/LoginRequest.java
@@ -12,6 +12,13 @@ public class LoginRequest {
     private String username;
     private String password;
 
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public LoginRequest() { }
+
     /**
      * Gets the username from the login request.
      *
