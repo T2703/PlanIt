c488520b8b33b6a8b446ca26d0d4e73f9b4c7eb7
Joshua Gutierrez Mejia <joshuagutierrezmejia@Joshuas-MacBook-Pro.local>
2023-12-03 09:31:50 -0600

    chore: refactored some tests

--- Frontend/app/src/androidTest/java/UI_Testing/NavbarTest.java
+++ Frontend/app/src/androidTest/java/UI_Testing/NavbarTest.java
@@ -1,25 +1,22 @@
 package UI_Testing;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 
 import com.example.myapplication.R;
 
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.allOf;
 
 import android.view.View;
 
@@ -36,26 +33,31 @@ public class NavbarTest {
     public ActivityScenarioRule<HomePage> activityRule = new ActivityScenarioRule<>(HomePage.class);
 
     @Test
-    public void homepageToCalendarToMessagesToProfile() {
-        // Click on the menu button to open the popup menu
-        Espresso.onView(withId(R.id.calendar_button_nav)).perform(ViewActions.click());
-        Espresso.onView(withId(R.id.message_button)).perform(ViewActions.click());
-        Espresso.onView(withId(R.id.profile_button)).perform(ViewActions.click());
-        Espresso.onView(withId(R.id.save_changes_button)).check(matches(isDisplayed()));
+    public void navigateToHomePage() {
+        onView(withId(R.id.home_button)).perform(click());
+        onView(withId(R.id.title)).check(matches(withText("Home")));
     }
 
     @Test
-    public void homepageToCalendarToHomepage() {
-        // Click on the menu button to open the popup menu
-        Espresso.onView(withId(R.id.calendar_button_nav)).perform(ViewActions.click());
-        Espresso.onView(withId(R.id.home_button)).perform(ViewActions.click());
-        Espresso.onView(withId(R.id.drawerLayout)).check(matches(isDisplayed()));
+    public void navigateToCalendarPage() {
+        onView(withId(R.id.calendar_button_nav)).perform(click());
+        onView((withId(R.id.menu_calendar_button))).check(matches(isDisplayed()));
     }
 
     @Test
-    public void homepageToProfile() {
-        Espresso.onView(withId(R.id.profile_button)).perform(ViewActions.click());
-        Espresso.onView(withId(R.id.profileImageView)).check(matches(isDisplayed()));
+    public void navigateToTeamPage() {
+        onView(withId(R.id.message_button)).perform(click());
+        
     }
 
+    @Test
+    public void navigateToProfilePage() {
+        onView(withId(R.id.profile_button)).perform(click());
+
+    }
+
+    public void navigateBackToHomepage() {
+        onView(withId(R.id.calendar_button_nav)).perform(click());
+        onView(withId(R.id.home_button)).perform(click());
+    }
 }
