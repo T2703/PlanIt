a65e537732ed1a60aaad2d12b5447d292461abe6
Joshua Gutierrez Mejia <joshuagutierrezmejia@josh1015.student.iastate.edu>
2023-12-04 19:20:30 -0600

    fix: change edit event page

--- Frontend/app/src/main/java/profile/LoginFormPage.java
+++ Frontend/app/src/main/java/profile/LoginFormPage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
