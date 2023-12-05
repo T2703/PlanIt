b3ce8955cfbbd4265959b71a88ebd5f497b1ca4e
nt0307e <nt0307e@iastate.edu>
2023-12-05 17:26:32 -0600

    fix: link

--- Frontend/app/src/main/java/messages/MessageView.java
+++ Frontend/app/src/main/java/messages/MessageView.java
@@ -30,7 +30,7 @@ public class MessageView extends AppCompatActivity implements WebSocketListener 
     /**
      * The url for the messaging.
      */
-    private String MESSAGE_URL = "ws://localhost:8080/chat/";
+    private String MESSAGE_URL = "ws://10.0.2.2:8080/chat/";
 
     /**
      * The button for sending message.
