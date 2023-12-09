0204e9a535a50cf090b0c307c47a7e8497262db9
Joshua Gutierrez Mejia <joshuagutierrezmejia@josh1015.student.iastate.edu>
2023-12-08 23:08:06 -0600

    chore: added schedule compare

--- Frontend/app/src/main/java/groups/TeamAvailability.java
+++ Frontend/app/src/main/java/groups/TeamAvailability.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -20,18 +22,23 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.myapplication.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -45,15 +52,20 @@ public class TeamAvailability extends AppCompatActivity {
     private TextView textView;
 
     private String group_id;
-    private static final String URL= "http://coms-309-024.class.las.iastate.edu:8080/compareStandard/"; //+ group_id + "/dates"
+    private static final String URL= "http://coms-309-024.class.las.iastate.edu:8080/compareStandard/";
 
-    private String result ="";
     private final Context teamAvailabilityPageContext = this;
 
     private EditText event_start_date2;
     private EditText event_end_date2;
     private EditText event_start_time2;
     private EditText event_end_time2;
+    private LinearLayout team_availability;
+    Context context = this;
+
+    Button goToCreateEvent;
+    ImageButton backButton;
+
 
 
     @Override
@@ -61,13 +73,14 @@ public class TeamAvailability extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_availability);
 
-        // Initialize
-        //context = this;
         group_id = getIntent().getStringExtra("groupId");
         event_start_date2 = findViewById(R.id.start_date_input2);
         event_end_date2 = findViewById(R.id.end_date_input2);
         event_start_time2 = findViewById(R.id.start_time_input2);
         event_end_time2 = findViewById(R.id.end_time_input2);
+        team_availability = findViewById(R.id.teamAvailability);
+        goToCreateEvent = findViewById(R.id.goToCreateEventPage);
+        backButton = findViewById(R.id.back_button_two2);
 
         Button see_Availability = findViewById(R.id.see_Availability);
 
@@ -104,10 +117,9 @@ public class TeamAvailability extends AppCompatActivity {
                 }
             });
 
-        see_Availability.setOnClickListener(new View.OnClickListener() {       // SEE AVAILABILITY
+        see_Availability.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] dates = stripDateAndTime();
                 String event_type_value2 = "private";
                 String event_name_value2 = "dateRange";
                 String event_location_value2 = "NA";
@@ -121,12 +133,27 @@ public class TeamAvailability extends AppCompatActivity {
                 String event_start_Combined = DateAndTimeHelper.combineDateAndTime(event_start_date_value, event_start_time_value);
                 String event_end_Combined = DateAndTimeHelper.combineDateAndTime(event_end_date_value, event_end_time_value);
 
-                getRequest(event_name_value2, event_description_value2, event_location_value2, event_type_value2, event_start_Combined, event_end_Combined);
+                putRequest(event_name_value2, event_description_value2, event_location_value2, event_type_value2, event_start_Combined, event_end_Combined);
+            }
+        });
+
+        goToCreateEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TeamAvailability.this, CreateEventPage.class);
+                TeamAvailability.this.startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
 
-    private void getRequest(String eventNameValue, String eventDescriptionValue, String eventLocationValue, String eventTypeValue, String eventStartDateValue, String eventEndDateValue){
+    private void putRequest(String eventNameValue, String eventDescriptionValue, String eventLocationValue, String eventTypeValue, String eventStartDateValue, String eventEndDateValue){
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JSONObject body = new JSONObject();
 
@@ -137,9 +164,6 @@ public class TeamAvailability extends AppCompatActivity {
             body.put("type", eventTypeValue);
             body.put("startDate", eventStartDateValue);
             body.put("endDate", eventEndDateValue);
-
-            System.out.println(body);
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -149,16 +173,21 @@ public class TeamAvailability extends AppCompatActivity {
                 URL+ group_id + "/dates",
                 body,
                 new Response.Listener<JSONObject>() {
-
-
                     @Override
                     public void onResponse(JSONObject response) {
-
-                        //Log.d("result", response);
-
                         try {
-                            textView = findViewById(R.id.response);
-                            textView.setText(response.get("message").toString());
+                            System.out.println(response.getString("message"));
+
+                            TextView textView = new TextView(context);
+                            textView.setText(response.getString("message"));
+
+                            int paddingValueInPixels = (int) getResources().getDimension(R.dimen.textview_padding);
+                            textView.setPadding(0, paddingValueInPixels, 0, paddingValueInPixels);
+
+                            team_availability.addView(textView);
+
+                            goToCreateEvent.setVisibility(View.VISIBLE);
+
                         } catch (Exception e) {
                             throw new RuntimeException(e);
                         }
@@ -175,17 +204,21 @@ public class TeamAvailability extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
-    private String[] stripDateAndTime() {
-        String start_date = event_start_date2.getText().toString();
-        String end_date = event_end_date2.getText().toString();
-        String start_time = event_start_time2.getText().toString();
-        String end_time = event_end_time2.getText().toString();
+    private String[] parseResponse(String response) {
+        String message = response.substring(response.indexOf(":") + 2, response.lastIndexOf("\"")).trim();
 
-        String startDate = start_date + "T" + start_time + ":00.000+00:00";
-        String endDate = end_date + "T" + end_time + ":00.000+00:00";
+        String[] dateRanges = message.split("\n");
 
-        return new String[]{startDate, endDate};
+        List<String> dateRangesList = new ArrayList<>();
+
+        for (String dateRange : dateRanges) {
+            dateRangesList.add(dateRange.trim());
+        }
+
+        String[] resultArray = dateRangesList.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(resultArray));
+
+        return resultArray;
     }
-
-
 }
